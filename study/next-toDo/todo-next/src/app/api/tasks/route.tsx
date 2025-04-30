6608154999364dad2dd761c816d0beed7f7e31a3
Kirill <kirillsavorin@gmail.com>
2025-04-30 18:57:17 +0200

    Route test

---
+++ study/next-toDo/todo-next/src/app/api/tasks/route.tsx
@@ -0,0 +1,27 @@
+import { TaskItem } from "@/types";
+import { NextRequest, NextResponse } from "next/server";
+
+const fakeDatabase: TaskItem[] = [
+  {
+    label: 'Learn next',
+    completed: false,
+  }
+];
+
+export function GET() {
+  return NextResponse.json({
+    data: fakeDatabase,
+  });
+}
+
+export async function POST(req: NextRequest) {
+  const newTask = await req.json();
+
+  fakeDatabase.push(newTask);
+
+  return NextResponse.json({
+    data: newTask
+  }, {
+    status: 201,
+  });
+}
